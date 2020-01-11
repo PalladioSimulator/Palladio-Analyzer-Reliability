4aabfa8d0e2894ba2c4553861185ac8a0c2455bf
Stephan Seifermann <stephan.seifermann@kit.edu>
2020-01-11 18:05:25 +0100

    Renamed imported solver bundle
    From: org.palladiosimulator.solver.reliability
    To:   org.palladiosimulator.reliability.solver

---
+++ bundles/org.palladiosimulator.reliability.solver/src/org/palladiosimulator/reliability/solver/SolverPlugin.java
@@ -0,0 +1,14 @@
+package org.palladiosimulator.reliability.solver;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class SolverPlugin extends AbstractUIPlugin {
+
+    /**
+     * The plug-in ID.
+     */
+    public static final String PLUGIN_ID = "org.palladiosimulator.reliability.solver";
+}
